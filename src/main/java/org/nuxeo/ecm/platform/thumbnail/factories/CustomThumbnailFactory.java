b9e069642e5a8028d16c59d27d5050f0cebb0394
Vincent Dutat <vdutat@nuxeo.com>
2016-05-26 16:48:20 -0700

    nx 7.10

--- src/main/java/org/nuxeo/ecm/platform/thumbnail/factories/CustomThumbnailFactory.java
+++ src/main/java/org/nuxeo/ecm/platform/thumbnail/factories/CustomThumbnailFactory.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     vdutat
+ */
 package org.nuxeo.ecm.platform.thumbnail.factories;
 
 import java.io.IOException;
@@ -8,9 +26,9 @@ import javax.faces.context.FacesContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.Blob;
-import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.PropertyException;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.api.thumbnail.ThumbnailFactory;
 import org.nuxeo.ecm.platform.thumbnail.ThumbnailConstants;
@@ -21,13 +39,13 @@ public class CustomThumbnailFactory implements ThumbnailFactory {
     private static final Log LOG = LogFactory.getLog(CustomThumbnailFactory.class);
 
     @Override
-    public Blob getThumbnail(DocumentModel doc, CoreSession session) throws ClientException {
+    public Blob getThumbnail(DocumentModel doc, CoreSession session) {
         String propertyName = Framework.getProperty("nuxeo.thumbnail." + doc.getType() + ".metadata", ThumbnailConstants.THUMBNAIL_PROPERTY_NAME);
 //        propertyName = "SUPNXP-14786_sch_imdc_user:photo";
         Blob thumbnailBlob = null;
         try {
             thumbnailBlob = (Blob) doc.getPropertyValue(propertyName);
-        } catch (ClientException e) {
+        } catch (PropertyException e) {
             LOG.warn("Could not fetch the thumbnail blob from property " + propertyName, e);
         }
         if (thumbnailBlob == null) {
--- src/main/java/org/nuxeo/ecm/platform/thumbnail/listener/CustomCheckBlobUpdateListener.java
+++ src/main/java/org/nuxeo/ecm/platform/thumbnail/listener/CustomCheckBlobUpdateListener.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     vdutat
+ */
 package org.nuxeo.ecm.platform.thumbnail.listener;
 
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.BEFORE_DOC_UPDATE;
@@ -6,7 +24,6 @@ import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_CREATED;
 import java.util.Arrays;
 
 import org.apache.commons.lang.StringUtils;
-import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.model.Property;
 import org.nuxeo.ecm.core.event.Event;
@@ -20,7 +37,7 @@ import org.nuxeo.runtime.api.Framework;
 public class CustomCheckBlobUpdateListener implements EventListener {
 
     @Override
-    public void handleEvent(Event event) throws ClientException {
+    public void handleEvent(Event event) {
         EventContext ec = event.getContext();
         if (!(ec instanceof DocumentEventContext)) {
             return;
